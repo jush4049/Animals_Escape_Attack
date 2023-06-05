04747eae926d347ed92ef6997f7f3cb86d6fba80
Ju Sang Hyun <96518656+jush4049@users.noreply.github.com>
2023-06-05 13:07:30 +0900

    edit

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
     public float speed = 10.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);

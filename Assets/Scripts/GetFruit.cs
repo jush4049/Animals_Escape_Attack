dc2fc64dfc290d47a87e9d00feb1b4fe72b0dedc
Ju Sang Hyun <96518656+jush4049@users.noreply.github.com>
2023-06-05 13:08:16 +0900

    edit

--- Assets/Scripts/GetFruit.cs
+++ Assets/Scripts/GetFruit.cs
@@ -13,15 +13,4 @@ public class GetFruit : MonoBehaviour
             Destroy(FruitApple);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
